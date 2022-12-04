11b7f726b9631c7466329e273a5edb7d540065e7
Muhammad Ilham Kurniawan <muh.ilhamkurniawan11@gmail.com>
2022-12-04 20:33:11 +0700

    Bagusin Desain

--- app/src/main/java/org/tensorflow/lite/codelabs/aksaraclassifier/MainActivity.kt
+++ app/src/main/java/org/tensorflow/lite/codelabs/aksaraclassifier/MainActivity.kt
@@ -2,6 +2,8 @@ package org.tensorflow.lite.codelabs.aksaraclassifier
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AbsListView
 import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
@@ -12,16 +14,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        canvasButton = findViewById(R.id.btnCanvas)
-        camGalButton = findViewById(R.id.btnCamGal)
-
-        canvasButton?.setOnClickListener{
-            startActivity(Intent(this, CanvasActivity::class.java))
-        }
-
-        camGalButton?.setOnClickListener{
-            startActivity(Intent(this, CamGalActivity::class.java))
-        }
+    }
+    fun canva(view: View?) {
+        startActivity(Intent(this, CanvasActivity::class.java))
+    }
+    fun camera(view: View?) {
+        startActivity(Intent(this, CamGalActivity::class.java))
     }
 }
